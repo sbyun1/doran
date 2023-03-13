436473e9c5cb64c353b80b010afea6fa2dc4e696
Subin Byun <sbyun95@gmail.com>
2023-03-13 15:45:53 +0900

    reactInit3

--- src/main/java/com/doran/doran/controller/MainController.java
+++ src/main/java/com/doran/doran/controller/MainController.java
@@ -3,10 +3,13 @@ package com.doran.doran.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class MainController {
-    @GetMapping("/test")
-    public String hello(){
-        return "test";
+    @GetMapping("/hello")
+    public List<String> hello(){
+        return Arrays.asList("안녕하세요","Hello");
     }
 }
