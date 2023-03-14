05acac636e56bdbf852b11e53e49145ff1ce71de
junmipark <junmipark1@gmail.com>
2023-03-15 07:00:17 +0900

    add CSS Event on Category Buttons & Product Option Buttons

--- src/main/reactjs/src/components/Main.js
+++ src/main/reactjs/src/components/Main.js
@@ -3,11 +3,13 @@ import '../resources/css/main.css';
 import '../resources/js/main.js';
 import axios from "axios";
 import {string} from "bfj/src/events";
+import {atRule} from "postcss";
 
 function Main() {
     const [products, setProducts] = useState([]);
     const [categories, setCategroies] = useState([]);
     const [keyword, setKeyword] = useState('');
+    const [currentCategory, setCurrentCategory] = useState(0);
 
     useEffect(() => {
         axios.get('/menu/init')
@@ -19,6 +21,7 @@ function Main() {
 
     const clickCategory = (event) => {
         const categoryId = event.target.getAttribute('data-key');
+        setCurrentCategory(categoryId);
 
         axios.get('/menu/findByCategory?categoryId=' + categoryId)
             .then(response => {
@@ -26,21 +29,33 @@ function Main() {
             });
     }
 
-    const findByKeyword = () => {
-        axios.get('/menu/findByKeyword?keyword=' + keyword)
-            .then(response => {
-                setProducts(response.data);
-            })
+    const isSelected = (categoryId) => {
+        if (currentCategory == categoryId)
+            return true;
+        else
+            return false;
     }
+
+    const
+        findByKeyword = () => {
+            axios.get('/menu/findByKeyword?keyword=' + keyword)
+                .then(response => {
+                    setProducts(response.data);
+                })
+            setKeyword('');
+        }
 
     return (
         <div className="main-container">
             <div className="product-category">
-                <span className="option-selected" data-key="0"
+                <span className={isSelected(0) ? "option-selected" : null}
+                      data-key="0"
                       onClick={clickCategory}>전체</span>
                 {
                     categories.map(category => {
-                        return <span key={category.categoryId} data-key={category.categoryId}
+                        return <span className={isSelected(category.categoryId) ? "option-selected" : null}
+                                     key={category.categoryId}
+                                     data-key={category.categoryId}
                                      onClick={clickCategory}>{category.categoryName}</span>
                     })
                 }
@@ -70,12 +85,18 @@ function Main() {
 }
 
 function Product({product}) {
+    const isSingle = (optionLength) => {
+        if (optionLength == 1)
+            return true;
+        return false;
+    }
+    
     return (
         <div className="product-figure">
             <div className="item-info">
                 <span>{product.categoryName}</span>
                 <span className="item-name">{product.productName}</span>
-                <div className="item-options">
+                <div className={`item-options ${isSingle(product.productOptions.length) ? "single" : null}`}>
                     {
                         product.productOptions.map(option => {
                             return <ProductOption key={option.optionId} data-key={option.optionId}
