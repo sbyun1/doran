b9a1392f02940df45f649028cb60a7cf77f4b30f
= <=>
2023-03-21 16:21:30 +0900

    update Codes - register 'Payement' Route in Router

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,6 +1,7 @@
 import Main from "./components/Main.js";
 import Cart from './components/Cart.js';
 import Event from './components/Event.js';
+import Payment from './components/Payment.js'
 import Header from './components/common/Header.js';
 import {BrowserRouter, Route, Routes, Link, Switch} from "react-router-dom";
 
@@ -13,6 +14,7 @@ function App() {
                     <Route exact path="/" element={<Main/>}/>
                     <Route exact path="/cart" element={<Cart/>}/>
                     <Route exact path="/event" element={<Event/>}/>
+                    <Route exact path="/payment" element={<Payment/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
--- src/main/reactjs/src/components/Payment.js
+++ src/main/reactjs/src/components/Payment.js
@@ -1,5 +1,7 @@
 function Payment() {
-
+    return (
+    <div></div>
+    )
 }
 
 export default Payment;
