5244a7d247e3ab94ddf18eab116a5b25f84f0ef3
junmipark <junmipark1@gmail.com>
2023-04-21 04:51:21 +0900

    update Pages - Order Info. Form (in progress)

--- src/main/reactjs/src/components/order/OrderConfirm.js
+++ src/main/reactjs/src/components/order/OrderConfirm.js
@@ -1,8 +1,49 @@
 import '../../resources/css/order/confirm.css'
+import {useState} from "react";
 
 function OrderConfirm() {
+    const [auth, setAuth] = useState(false);
+
     return (
-        <div className="main-container">
+        <div className="main-container order-confirm-back">
+            <Before/>
+        </div>
+    )
+}
+
+function Before() {
+    return (
+        <div className={"order-confirm"}>
+            <div className={"order-top"}>
+                <span>주문 조회</span>
+                <span>CAFE DORAN</span>
+            </div>
+            <div className={"order-check"}>
+                <div>
+                    <span>주문번호</span>
+                    <input name={"orderSeq"} inputMode={"numeric"}/>
+                </div>
+                <div>
+                    <span>주문자명</span>
+                    <input name={"orderName"}/>
+                </div>
+                <div>
+                    <span>비밀번호</span>
+                    <input name={"orderPassword"} type={"password"} inputMode={"numeric"}/>
+                </div>
+                <span className={"order-check-desc"}>
+                    * 주문 정보 오 입력 시 주문 상태 확인 및 취소가 어려울 수 있으니
+                    다시 한번 확인해 주시기 바랍니다.
+                </span>
+                <input className={"style-button-confirm"} type={"button"} value={"조회하기"}/>
+            </div>
+        </div>
+    )
+}
+
+function After() {
+    return (
+        <>
             <div className="order-top">
                 <span>주문정보</span>
             </div>
@@ -57,7 +98,7 @@ function OrderConfirm() {
                     <span>상품이 준비 완료되었습니다.</span>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
